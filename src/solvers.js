cf48e985c49b020d10e2a8ccbb243af238f38e33
mattupham <mcupham@gmail.com>
2017-12-07 12:31:58 -0800

    Refactor helper function for rook solution
    also working on using helper function to make nrook solution

--- src/solvers.js
+++ src/solvers.js
@@ -14,39 +14,39 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 
+window.findFirstSolutionForGivenBoard = function(board, rowIndex, colIndex, n) {
+  //finds first solution given a specific board
+
+  //if empty board, then default row/col index are 0, else they are passed in
+  // put rook (default first square)
+  //add new piece on current location
+  board.togglePiece(rowIndex, colIndex);
+  var newBoard = board;
+  //run conflicts function on current board
+  var hasConflict = newBoard.hasAnyRooksConflicts();
+  //exit if we get to the end of the board
+  if (rowIndex === n - 1 && colIndex === n - 1) {
+    return newBoard.rows();
+  } 
+  if (hasConflict) {
+    //if true, there is a conflict, take piece off
+    newBoard.togglePiece(rowIndex, colIndex);
+  }
+  //move on to next square
+  //if at end of row
+  if (colIndex === n - 1) {
+    //go down (add 1 to row), reset column to 0
+    return findFirstSolutionForGivenBoard(newBoard, rowIndex + 1, 0, n);
+  } else {
+    //if not at end of row, go right
+    return findFirstSolutionForGivenBoard(newBoard, rowIndex, colIndex + 1, n);
+  }
+}; 
 
 window.findNRooksSolution = function(n) {
   // create game board
   var myGame = new Board({'n': n});
-  var placeRook = function(board, rowIndex, colIndex) {
-    //if empty board, then default row/col index are 0, else they are passed in
-    // put rook (default first square)
-    //add new piece on current location
-    board.togglePiece(rowIndex, colIndex);
-    var newBoard = board;
-    //run conflicts function on current board
-    var hasConflict = newBoard.hasAnyRooksConflicts();
-    //exit if we get to the end of the board
-    if (rowIndex === n - 1 && colIndex === n - 1) {
-      return newBoard.rows();
-    } 
-    if (hasConflict) {
-      //if true, there is a conflict, take piece off
-      newBoard.togglePiece(rowIndex, colIndex);
-    }
-    //move on to next square
-    //if at end of row
-    if (colIndex === n - 1) {
-      //go down (add 1 to row), reset column to 0
-      return placeRook(newBoard, rowIndex + 1, 0);
-    } else {
-      //if not at end of row, go right
-      return placeRook(newBoard, rowIndex, colIndex + 1);
-    }
-  }; 
-  
-  var solution = placeRook(myGame, 0, 0);
-
+  var solution = window.findFirstSolutionForGivenBoard(myGame, 0, 0, n);
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
@@ -55,28 +55,31 @@ window.findNRooksSolution = function(n) {
 window.countNRooksSolutions = function(n) {
   var solutionCount = 0;
   
-  //constant
-  //rook count always needs to be 4 for a valid solution
-    
-    
-  //variable
-  //starting location of each invocation of findNRooksSolution
-    
-  //next grid needs to save index of previously valid placed piece
-  //1st board: nook1 (0,0), nook2 (1,1)
-  //2nd board: nook1 (0,0), nook2 (1,2) 
-  //--> 2nd board nook2 remembers 1st board nook2 location, 
-  //starts placement after 1st board nook2 location
-  
-  //returns a number recursively like an iterative .reduce function
-  //return func(2) + 2
-  
-  
-  
-  
-  
-  
+  //create a board
+  //run placeRook on that board with updated indices
+    //starts scanning on space after previously placed rook
+  //
 
+  //inner recursive function
+    // placeRook(board, rowIndex, colIndex) 
+      
+      //loop over row index col indeed to get solutions givena  board state
+        // var createSolution = function(board, rowIndex, colIndex) {
+        //   placeRook(board, rowIndex, colIndex);
+        //   solutionCount++;
+        //   return placeRook(board, rowIndex + 1, colIndex + 1)
+        // }
+        // createSolution();
+
+      var createSolution = function(n, callback){
+        
+        callback();
+        solution++;
+        return createSolution(n, callback);
+      }
+
+      createSolution(n, placeRook(board, rowIndex, colIndex));
+  
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
